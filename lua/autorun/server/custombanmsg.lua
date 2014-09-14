9416f82c0016cdd72f7277839dae78d470c15d7e
edgarasf123 <edgarasf123@gmail.com>
2014-09-14 01:31:34 -0700

    Global tables
    * Added global table
    * Optimized the code (https://github.com/edgarasf123/CustomBanMessage/pull/1)

--- lua/autorun/server/custombanmsg.lua
+++ lua/autorun/server/custombanmsg.lua
@@ -1,3 +1,4 @@
+CBM = CBM or {} -- Global table
 -- ----------------------------------------------------------
 -- ----------------------- START_CONFIG --------------------- 
 -- ----------------------------------------------------------
@@ -15,7 +16,7 @@ user_steamid    The current user steamid.
 user_ip         The IP of the user.
 ]]
 
-local msgBan = 
+CBM.msgBan = 
 [[================================
       BANNED FROM SERVER
 ================================
@@ -34,7 +35,7 @@ user_name     The current user name.
 user_steamid  The current user steamid.
 user_ip       The IP of the user.
 ]]
-local msgWrongPass = [[Password is incorrect!]]
+CBM.msgWrongPass = [[Password is incorrect!]]
 
 --[[
 ===== WhiteList Message Variables =====
@@ -42,19 +43,21 @@ user_name      The current user name.
 user_steamid   The current user steamid.
 user_ip        The IP of the user.
 ]]
-local msgWhitelist = [[You're not in the whitelist.]]
+CBM.msgWhitelist = [[You're not in the whitelist.]]
 
 
 
 -- WhiteList Stuff
-local whitelistEnabled = true
-local whitelist = {
+CBM.whitelistEnabled = true
+CBM.whitelist = {
 	"STEAM_0:0:0",
 	"STEAM_0:0:0",
 }
 -- ----------------------------------------------------------
 -- ----------------------- END_CONFIG ----------------------- 
 -- ----------------------------------------------------------
+
+-- Helper Functions
 local function SecondsFormat(X)
     if X < 0 then return "" end
     local date = os.date("!*t", X)
@@ -72,16 +75,18 @@ local function tosteamid(cid)
   local sid = "STEAM_0:" .. a .. ":" .. (a == 1 and b -1 or b)
   return sid
 end
-hook.Add("CheckPassword", "CheckPasswordCustom", function( steamID64, ipAddress, svPass, clPass, name )
-	-- Inline logic FTW
+
+-- CheckPassword Hook (the main proccess of this addon)
+hook.Add("CheckPassword", "CBM_Check", function( steamID64, ipAddress, svPass, clPass, name )
+	local msg, params = "", {}
 	local steamID = tosteamid(steamID64)
-	if ULib and ULib.bans[ steamID ] then
+	if ULib and ULib.bans[ steamID ] then -- Check if user is banned by ULX.
 		local banData = ULib.bans[ steamID ]
 		local banDataUnban = tonumber(banData.unban)
 		local banDataTime = tonumber(banData.time)
 		
+		msg = CBM.msgBan
 		
-		local params = {}
 		params["ban_time"] = banDataTime and (banDataTime > 0 and os.date('%c', banDataTime)) or "Undefined"
 		params["ban_unban"] = banDataUnban and (banDataUnban > 0 and os.date('%c', banDataUnban) or "Permanent") or "Undefined"
 		params["ban_timeleft"] = banDataUnban and ( banDataUnban > 0 and SecondsFormat( banDataUnban - os.time() ) or "Permanent" ) or "Undefined"
@@ -92,35 +97,25 @@ hook.Add("CheckPassword", "CheckPasswordCustom", function( steamID64, ipAddress,
 		params["ban_admin"] = banData.admin or "(Console)"
 		params["ban_name"] = banData.name or name or "Undefined"
 		params["user_name"] = name or "Undefined"
+	elseif #svPass > 0 and clPass ~= svPass then -- Check if user has entered correct password when sv_password is set.
+		msg = CBM.msgWrongPass
 		
-		local msg = msgBan
-		for k,v in pairs(params) do
-			msg = string.Replace(msg, "{{ "..k.." }}", v)
-		end
-		return false, msg
-	elseif #svPass > 0 and clPass ~= svPass then
-		local params = {}
 		params["user_pass"] = clPass or ""
 		params["user_steamid"] = steamID or "Undefined"
 		params["user_ip"] = ipAddress or "Undefined"
 		params["user_name"] = name or "Undefined"
+	elseif CBM.whitelistEnabled and not table.HasValue(CBM.whitelist, steamID) then -- Check if user in whitelist when whitelist is enabled.
+		msg = CBM.msgWhitelist
 		
-		local msg = msgWrongPass
-		for k,v in pairs(params) do
-			msg = string.Replace(msg, "{{ "..k.." }}", v)
-		end
-		return false, msg
-	elseif whitelistEnabled and not table.HasValue(whitelist, steamID) then
-		local params = {}
 		params["user_steamid"] = steamID or "Undefined"
 		params["user_ip"] = ipAddress or "Undefined"
 		params["user_name"] = name or "Undefined"
-		
-		local msg = msgWhitelist
-		for k,v in pairs(params) do
-			msg = string.Replace(msg, "{{ "..k.." }}", v)
-		end
-		return false, msg
+	else -- Since user has passed all checks, allow user to enter the server.
+		return true
 	end
-	return true
+	
+	for k,v in pairs(params) do
+		msg = string.Replace(msg, "{{ "..k.." }}", v)
+	end
+	return false, msg
 end)
